b040b22a7af4cf0c726d309990cfa3026238d3f9
Tzahi Vidas <tzahi.vidas@gmail.com>
2018-01-15 00:32:12 -0500

    Remove preload settings

--- public/main.js
+++ public/main.js
@@ -151,14 +151,7 @@ function openFile() {
 }
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 900,
-    height: 680
-    // webPreferences: {
-    //   nodeIntegration: false,
-    //   preload: path.join(__dirname, 'preload.js')
-    // }
-  });
+  mainWindow = new BrowserWindow({width: 900, height: 680});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
   autoUpdater.checkForUpdates();
